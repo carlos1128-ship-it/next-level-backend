ea0cdb70ecafd492a70ad5564e36643c2a1e489a
carlos1128-ship-it <carloshenriquebarbosa1128@gmail.com>
2026-02-19 19:11:12 -0300

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'https://next-level-front-6rn4hgy9e-carlos1128-ship-its-projects.vercel.app',
-      'http://localhost:3000',
-    ],
+    origin: true,
     credentials: true,
   });
 
